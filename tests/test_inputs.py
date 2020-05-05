043aa201d6a6cd29152156a01d1edbced3e2bd73
harupy <hkawamura0130@gmail.com>
2020-05-05 13:13:21 +0900

    rename

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -23,7 +23,7 @@ def test_input_schema_matches_action_yaml(action_yaml, input_schema):
     assert set(input_schema.keys()) == set(action_yaml["inputs"].keys())
 
 
-def test_casting_action_inputs_does_not_fail(action_yaml, input_schema):
+def test_casting_default_action_inputs_does_not_fail(action_yaml, input_schema):
     for name, inp in action_yaml["inputs"].items():
         if "default" not in inp:
             continue
